384f8253d9525de9fc1be05ee0b7b29dbd03ba5e
wiem <nouirawiem@gmail.com>
2020-02-27 17:46:49 +0100

    partie frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppTodo';
+  title = 'Test';
+   books =
+    [
+     {
+            name: "Sue Shei",
+            email: "sue.shei@example.com",
+            phone: "0123456789"
+            
+            },
+			{
+            name: "Sue Shei",
+            email: "sue.shei@example.com",
+            phone: "0123456789"
+            
+            },
+			{
+            name: "Sue Shei",
+            email: "sue.shei@example.com",
+            phone: "0123456789"
+            
+            },
+			{
+            name: "Sue Shei",
+            email: "sue.shei@example.com",
+            phone: "0123456789"
+            
+            },
+			{
+            name: "Sue Shei",
+            email: "sue.shei@example.com",
+            phone: "0123456789"
+            
+            },
+			{
+            name: "Sue Shei",
+            email: "sue.shei@example.com",
+            phone: "0123456789"
+            
+            }
+	
+    ]
+	
+alphabets=['A','B','C']
+	
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,5 +59,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
- * APPLICATION IMPORTS
+ * APPLICATION IMPORTS  verions navigateur
  */
